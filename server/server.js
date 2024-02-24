3b99edb4131e61939b4748c62711fba3b6a4a785
arthdarji352 <arthdarji20@gmail.com>
2024-02-24 15:34:52 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectToDB from "./config/db.js";
 import cors from "cors";
 import routes from "./routes/TodoRoute.js";
+import path from "path";
 
 const app = express();
 app.use(express.json());
@@ -9,11 +10,17 @@ app.use(cors());
 
 connectToDB();
 
+const __dirname = path.resolve();
+
 const PORT = 8000;
 
 //routes
-app.use(routes);
+app.use("/api", routes);
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
 });
